a12fd59f45fcdac66abca08eed56d2d7f0b7de5b
unknown <rvcarcueva2@student.apc.edu.ph>
2024-02-17 23:16:30 +0800

    added favicon

--- ci4/app/Views/pages/home.php
+++ ci4/app/Views/pages/home.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <title>Cel - UI / Ux Desinger</title>
-  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <link rel="icon" href="favicon1.ico" type="image/x-icon">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">

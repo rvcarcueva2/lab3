3ae3dda43ec6cda29625200c037801330c956c02
unknown <rvcarcueva2@student.apc.edu.ph>
2024-02-27 16:24:35 +0800

    added news model

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -7,8 +7,14 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-
+use App\Controllers\News;
 use App\Controllers\Pages;
+
+$routes->get('news', [News::class, 'index']);
+$routes->get('news/new', [News::class, 'new']);
+$routes->post('news', [News::class, 'create']);           
+$routes->get('news/(:segment)', [News::class, 'show']);
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
+
